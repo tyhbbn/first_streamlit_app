d955063e4ef5bd53e7513468fe75023445750338
tyhbbn <141793071+tyhbbn@users.noreply.github.com>
2023-08-10 21:05:02 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,9 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
-streamlit.write('The user entered ', fruit_choice)
+streamlit.error("Please select a fruit to get information")
 else
 # import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
